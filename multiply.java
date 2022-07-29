8666815a749005946c19de7970a33e27faccd00b
vyankateshpachpohar <vyankateshpachpohar@gmail.com>
2022-07-29 15:12:05 +0530

    added multiply.java

---
+++ multiply.java
@@ -0,0 +1,9 @@
+Public Class multiply {
+
+        Run | Debug
+        public static void main(Sting[] args)
+            init a = 4;
+            init b = 2;
+
+            System.out.println("The sum is" = (a*b));
+        }
