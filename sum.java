c18831d47281f0b8221db52dd4f4d90baaefbdca
vyankateshpachpohar <vyankateshpachpohar@gmail.com>
2022-07-29 13:45:54 +0530

    initial commit

---
+++ sum.java
@@ -0,0 +1,9 @@
+Public Class Sum {
+
+        Run | Debug
+        public static void main(Sting[] args)
+            init a = 5;
+            init b = 6;
+
+            System.out.println(a+b);
+        }
